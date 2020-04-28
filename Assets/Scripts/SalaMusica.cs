2e92cba6238ac35439c6b4ff8cf283e1d3e81f5d
Pablo Martín Navarro <pablma13@ucm.es>
2020-04-29 00:20:20 +0200

    arreglo vista desde la sala de música

---
+++ Assets/Scripts/SalaMusica.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SalaMusica : MonoBehaviour
+{
+    public GameObject PertenenciasDelFantasma;
+    //public Vector3 secreto;
+    //private Vector3 paredNormal;
+
+    //private void Awake()
+    //{
+    //    paredNormal = GetComponent<Transform>().position;
+    //}
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Fantasma"))
+        {
+            PertenenciasDelFantasma.transform.position = new Vector3(PertenenciasDelFantasma.transform.position.x,
+                                                                      -0.75f,
+                                                                      PertenenciasDelFantasma.transform.position.z);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Fantasma"))
+        {
+            PertenenciasDelFantasma.transform.position = new Vector3(PertenenciasDelFantasma.transform.position.x,
+                                                                     0.5f,
+                                                                     PertenenciasDelFantasma.transform.position.z);
+        }
+    }
+}
